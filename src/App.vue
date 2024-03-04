1cffc56e54bf48dc19eb7a9455883d810f24537c
Aryans <quispeloayza18@hotmail.com>
2024-03-04 18:01:28 -0500

    fisrt commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import { ref ,onMounted } from 'vue';
+
+const monedas = ref([
+      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+      { codigo: 'MXN', texto: 'Peso Mexicano'},
+      { codigo: 'EUR', texto: 'Euro'},
+      { codigo: 'GBP', texto: 'Libra Esterlina'},
+  ])
+
+  const criptomonedas = ref([])
+
+  onMounted(()=>{
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+    fetch(url)
+     .then(response => response.json())
+     .then(({Data}) => criptomonedas.value = Data)
+  })
+</script>
+
+<template>
+  <div class="contenedor">
+    <h1 class="titulo">
+      Cotizador de<span>Criptomonedas</span>
+
+      <div class="contenido">
+        <form class="formulario" action="">
+          <div class="campo">
+            <label for="moneda">Moneda:</label>
+            <select name="" id="moneda">
+              <option value="">--Selecciona--</option>
+              <option v-for="moneda in monedas" :value="moneda.codigo">{{moneda.texto}}</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
