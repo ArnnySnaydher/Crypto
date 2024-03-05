2d42478f5b5d03a20dd2f0113b19d4dd8df5952e
Aryans <quispeloayza18@hotmail.com>
2024-03-05 14:34:52 -0500

    obteniendo cotizacion

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,52 @@
 <script setup>
 import Alerta from './Alerta.vue';
 
-import { ref ,onMounted ,reactive} from 'vue';
+import { ref, onMounted, reactive } from 'vue';
 
-const error=ref('')
+const error = ref('')
 
 const monedas = ref([
-      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
-      { codigo: 'MXN', texto: 'Peso Mexicano'},
-      { codigo: 'EUR', texto: 'Euro'},
-      { codigo: 'GBP', texto: 'Libra Esterlina'},
-  ])
+  { codigo: 'USD', texto: 'Dolar de Estados Unidos' },
+  { codigo: 'MXN', texto: 'Peso Mexicano' },
+  { codigo: 'EUR', texto: 'Euro' },
+  { codigo: 'GBP', texto: 'Libra Esterlina' },
+])
 
-  const criptomonedas = ref([])
-  const cotizar=reactive({
-    moneda:'',
-    criptomoneda:''
-  })
+const criptomonedas = ref([])
+const cotizar = reactive({
+  moneda: '',
+  criptomoneda: ''
+})
 
-  onMounted(()=>{
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    fetch(url)
-     .then(response => response.json())
-     .then(({Data}) => criptomonedas.value = Data)
-  })
+onMounted(() => {
+  const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+  fetch(url)
+    .then(response => response.json())
+    .then(({ Data }) => criptomonedas.value = Data)
+})
 
-  const cotizarCripto = ()=>{
-    //VAlidar que cotizar esta lleno
-    if(Object.values(cotizar).includes('')){
-      error.value='Todos los campos son obligatorios'
-      return
-    }
-
-    error.value=''
-
-    console.log('mmm')
-    
+const cotizarCripto = () => {
+  //VAlidar que cotizar esta lleno
+  if (Object.values(cotizar).includes('')) {
+    error.value = 'Todos los campos son obligatorios'
+    return
   }
+
+  error.value = ''
+
+  obtenerCotizacion()
+}
+
+const obtenerCotizacion = async () => {
+  try {
+    const {moneda,criptomoneda} =cotizar
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+      console.log(url)
+  } catch (error) {
+
+  }
+}
+
 </script>
 
 <template>
@@ -45,13 +55,13 @@ const monedas = ref([
       Cotizador de<span>Criptomonedas</span>
 
       <div class="contenido">
-        <Alerta v-if="error">{{error}}</Alerta>
+        <Alerta v-if="error">{{ error }}</Alerta>
         <form class="formulario" action="" @submit.prevent="cotizarCripto">
           <div class="campo">
             <label for="moneda">Moneda:</label>
             <select name="" id="moneda" v-model="cotizar.moneda">
               <option value="">--Selecciona--</option>
-              <option v-for="moneda in monedas" :value="moneda.codigo">{{moneda.texto}}</option>
+              <option v-for="moneda in monedas" :value="moneda.codigo">{{ moneda.texto }}</option>
             </select>
           </div>
 
@@ -59,7 +69,8 @@ const monedas = ref([
             <label for="crypto">Criptomonedas:</label>
             <select name="" id="crypto" v-model="cotizar.criptomoneda">
               <option value="">--Selecciona--</option>
-              <option v-for="crypto in criptomonedas" :value="crypto.CoinInfo.Name">{{crypto.CoinInfo.FullName}}</option>
+              <option v-for="crypto in criptomonedas" :value="crypto.CoinInfo.Name">{{ crypto.CoinInfo.FullName }}
+              </option>
             </select>
           </div>
 
