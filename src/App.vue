9fa5c75a773f6f67984c0a7a29ee615ffe5e08f0
Aryans <quispeloayza18@hotmail.com>
2024-03-05 22:47:03 -0500

    add composables

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 import Alerta from './components/Alerta.vue';
 import Spinner from './components/Spinner.vue';
 import useCripto from './composables/useCripto';
+import Cotizacion from './components/Cotizacion.vue'
 
 import { ref, reactive } from 'vue';
 
 
-const { monedas, criptomonedas, cargando, cotizacion, obtenerCotizacion, mostrarResultado } = useCripto()
+const { monedas, criptomonedas, cargando,cotizacion, obtenerCotizacion, mostrarResultado } = useCripto()
 
 const error = ref('')
 
@@ -59,22 +60,8 @@ const cotizarCripto = () => {
       </form>
       <Spinner v-if="cargando"></Spinner>
 
-      <div class="contenedor-resultado" v-if="mostrarResultado">
-        <h2>
-          Cotización
-        </h2>
-        <div class="resultado">
-          <img :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL" alt="imagen cripto">
-          <div>
-            <p>El precio es de: <span>{{ cotizacion.PRICE }} </span></p>
-            <p> Precio más alto del dia: <span>{{ cotizacion.HIGHDAY }}</span></p>
-            <p> Precio más bajo de hoy dia: <span>{{ cotizacion.LOWDAY }}</span></p>
-            <p> Variación últimas 24hrs: <span>{{ cotizacion.CHANGEPCT24HOUR }}%</span></p>
-            <p> Última actualización: <span>{{ cotizacion.LASTUPDATE }}</span></p>
+      <Cotizacion :cotizacion="cotizacion" v-if="mostrarResultado"></Cotizacion>
 
-          </div>
-        </div>
-      </div>
     </div>
 
   </div>
